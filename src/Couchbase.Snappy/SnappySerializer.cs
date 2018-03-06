4235e6fd40c0371771d0ebac28b88303b9e54b4c
Mike Goldsmith <goldsmith.mike@gmail.com>
2018-03-06 15:40:58 +0000

    Add Snappy compatible type transcoder

---
+++ src/Couchbase.Snappy/SnappySerializer.cs
@@ -0,0 +1,35 @@
+﻿using Couchbase.Core.Transcoders;
+using Couchbase.IO.Operations;
+using Snappy;
+
+namespace Couchbase.Snappy
+{
+    public class SnappyTranscoder : DefaultTranscoder
+    {
+        private const byte Snappy = 0x02; // todo: replace with Couchbase DateType enum
+
+        public override T Decode<T>(byte[] buffer, int offset, int length, Flags flags, OperationCode opcode)
+        {
+            if ((ushort) flags.DataFormat == Snappy)
+            {
+                var decodedBuffer = SnappyCodec.Uncompress(buffer);
+                var result = base.Decode<T>(decodedBuffer, offset, length, flags, opcode);
+                flags.DataFormat = DataFormat.Json; // reset dataformat back to JSON
+                return result;
+            }
+
+            return base.Decode<T>(buffer, offset, length, flags, opcode);
+        }
+
+        public override byte[] Encode<T>(T value, Flags flags, OperationCode opcode)
+        {
+            var buffer = base.Encode(value, flags, opcode);
+            if ((ushort) flags.DataFormat == Snappy)
+            {
+                return SnappyCodec.Compress(buffer);
+            }
+
+            return buffer;
+        }
+    }
+}
